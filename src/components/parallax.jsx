377e4915e25a5bae49848d0c8fccbf32a82b21dd
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-12-15 12:55:51 -0800

    stars + twinkling added to parallax

--- src/components/parallax.jsx
+++ src/components/parallax.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
-
+import '../sass/Parallax.scss';
 
 class MainParallax extends React.Component {
     render() {
         return (
             <Parallax vertical ref={(ref) => (this.parallax = ref)} pages={3}>
 
-                <ParallaxLayer offset={0} speed={1}  onClick={() => this.parallax.scrollTo(1)} style={{ background: 'black' }} />
+                <div className="stars">
+                    <div className="twinkling">
+                <ParallaxLayer offset={0} speed={1}  onClick={() => this.parallax.scrollTo(1)} />
+                    </div>
+                </div>
                 <ParallaxLayer offset={1} speed={1} onClick={() => this.parallax.scrollTo(2)} style={{ background: '#AF81FA' }} />
 
                 <ParallaxLayer offset={2} speed={0} factor={3} onClick={() => this.parallax.scrollTo(0)} style={{ backgroundColor: '#253237' }} />
