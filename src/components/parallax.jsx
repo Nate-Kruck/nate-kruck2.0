b09c7b9bb06858c342f9b3da9745ac48664ba6fa
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-12-15 16:58:38 -0800

    star background

--- src/components/parallax.jsx
+++ src/components/parallax.jsx
@@ -8,11 +8,13 @@ class MainParallax extends React.Component {
             <Parallax vertical ref={(ref) => (this.parallax = ref)} pages={3}>
 
                 <div className="stars">
-                    <div className="twinkling">
-                <ParallaxLayer offset={0} speed={1}  onClick={() => this.parallax.scrollTo(1)} />
-                    </div>
+                <div className="twinkling">
+                    <ParallaxLayer offset={0} speed={1}  onClick={() => this.parallax.scrollTo(1)} />
                 </div>
-                <ParallaxLayer offset={1} speed={1} onClick={() => this.parallax.scrollTo(2)} style={{ background: '#AF81FA' }} />
+                </div>
+
+                    <ParallaxLayer offset={1} speed={1} onClick={() => this.parallax.scrollTo(2)} />
+               
 
                 <ParallaxLayer offset={2} speed={0} factor={3} onClick={() => this.parallax.scrollTo(0)} style={{ backgroundColor: '#253237' }} />
 
