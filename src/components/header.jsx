0a9cbc86a2b265f4a41e9fd82343297a78f7a0e5
Nathan Kruckenberg <natekruck1990@gmail.com>
2021-01-07 15:01:30 -0800

    deleted unnecesary thumbnail component and add individual project links - cleaning up code

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
     return (
         <div className="header">
-            <div className="header header--name">
+            <div className="header header-name">
                 <h1>Nathan Town</h1>
             </div>
         </div>
