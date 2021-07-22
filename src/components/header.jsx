7c66472b93e46f4fdf8c5a1a05055092598fef66
Nathan Kruckenberg <natekruck1990@gmail.com>
2021-07-22 16:12:58 -0500

    Landing page (#10)
    * working on functionality when shrinking or expanding the page
    
    * deleted unnecesary thumbnail component and add individual project links - cleaning up code
    
    * added some cool components and changed up sass along with images
    
    * eslintcache

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Zoom from 'react-reveal/Zoom';
 
-
 class Header extends React.Component {
     render() {
         return (
@@ -11,16 +10,5 @@ class Header extends React.Component {
                         <h1>Nathan Town</h1>
                     </Zoom>
                 </div>
-
-function Header() {
-    return (
-        <div className="header">
-            <div className="header header-name">
-                <h1>Nathan Town</h1>
-
-            </div>
-        )
-    }
-}
 
 export default Header;
