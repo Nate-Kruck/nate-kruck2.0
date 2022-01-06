a13650e6bb50025452f774724e66f7a90d4512ad
Nathan Kruckenberg <natekruck1990@gmail.com>
2022-01-06 12:45:26 -0600

    fixing about page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
 import '../sass/About.scss';
-import ocean from '../videos/ocean.mp4';
+// import ocean from '../videos/ocean.mp4';
+import Bounce from 'react-reveal/Bounce';
 import RubberBand from 'react-reveal/RubberBand';
 import { useSpring } from 'react-spring';
 import { FullMenu } from '../components/navigation';
-
+import javascript from '../images/javascript.png';
+import react from '../images/react.png';
+import githubgradient from '../images/githubgradient.png';
+import html5 from '../images/javascript.png';
+import markdown from '../images/markdown.png';
+import java from '../images/java.png';
+import design from '../images/design.png';
 
 const About = () => {
     
@@ -17,14 +24,38 @@ const About = () => {
 
     return (
         <>
+        <div className="stars">
+        <div className="twinkling">
         <div className="container">
-            <video className="ocean" autoPlay loop muted>
-                <source src={ocean}type="video/mp4"/>
-            </video>
             
             <h1 className="about-me">About Me</h1>
 
-            <p>Hi! Im Nate Kruck, this is the start of my about page. I will add fun facts about me here so you can see how boring I am! lol jkjk. I love coding because it brings me joy. Bringing a webpage to life is a special skill and requires a ton of time.</p>
+            <h3>Hey, I'm 
+                <Bounce top cascade>
+                    <span className='name'> Nathan</span>
+                </Bounce>
+                </h3>
+                
+            <p>
+                Full stack software developer.
+            </p>
+            <p>
+               Currently working on personal projects, like my 
+               <a className="wedding-site" href='https://www.willowandnathan.com' alt="wedding" target="blank"><Bounce bottom cascade>wedding website.</Bounce>
+               </a> I enjoy bringing webpages to life and creating an enjoyable user experience. 
+            </p>
+            <div className='icon-group'>
+                <img className='tech-icons' src={javascript} alt='javascript'></img>
+                <img className='tech-icons' src={react} alt='react'></img>
+                <img className='tech-icons' src={githubgradient} alt='githubgradient'></img>
+                <img className='tech-icons' src={html5} alt='html5'></img>
+                <img className='tech-icons' src={markdown} alt='markdown'></img>
+                <img className='tech-icons' src={java} alt='java'></img>
+                <img className='tech-icons' src={design} alt='design'></img>
+            </div>
+
+        </div>
+        </div>
 
              <RubberBand>
               <button 
@@ -38,6 +69,7 @@ const About = () => {
             </RubberBand>
             <FullMenu style={fullMenuAnimation} />
         </div>
+
         </>
     )
 }
